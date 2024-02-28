149afa27f7e463deb14ad6f3fddfd454813d45e3
ZhaoyunMen <a1837712@adelaide.edu.au>
2024-02-28 20:48:01 +0800

    change

--- count.cpp
+++ count.cpp
@@ -3,7 +3,7 @@ int count(int array[], int n) {
     int Count = 0;
     for (int i = 0; i < n; ++i) {
         if (array[i] % 2 == 0) {
-            Count ++ array[i];
+            Count ++ ;
         }
     }
     return Count;
