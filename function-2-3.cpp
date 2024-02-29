1b71140be522d56db663e7abc790978e1fe80e61
ZhaoyunMen <a1837712@adelaide.edu.au>
2024-02-29 21:21:32 +0800

    change

--- function-2-3.cpp
+++ function-2-3.cpp
@@ -5,15 +5,19 @@ void two_five_nine(int array[], int n) {
     int num_twos = 0;
     int num_fives = 0;
     int num_nines = 0;
-    for (int i = 0; i < n; ++i) {
-        if (array[i] == 2){
-            ++num_twos;
-        }else if (array[i]==5){
-            ++numbers_fives;
-        }else if (array[i]==9){
-            ++num_nines;
+    for (int i = 0; i < n; ++i) 
+        switch (array[i]) {
+            case 2:
+                num_twos++;
+                break;
+            case 5:
+                num_fives++;
+                break;
+            case 9:
+                num_nines++;
+                break;
         }
-    }
     std::cout << "2:" << num_twos << ";5:" << num_fives << ";9:" << num_nines << ";" << std::endl;
 
-    }
+    
+}
