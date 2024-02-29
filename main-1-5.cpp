67a82693289c01c895eed34001d85ead5f2f00aa
ZhaoyunMen <a1837712@adelaide.edu.au>
2024-02-29 19:48:46 +0800

    pra1-5

---
+++ main-1-5.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+extern int count_evens(int number);
+int main(){
+    int number = 7;
+    std :: "the numberof even numbers is:" << count_evens(number)<< std :: endl;
+    return 0;
+}
