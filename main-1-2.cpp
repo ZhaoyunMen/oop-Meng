c3d414f14d8b6790c369317a86edd79e9ec64604
ZhaoyunMen <a1837712@adelaide.edu.au>
2024-02-29 19:09:08 +0800

    pra1-3

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-double array_mean(int array[], int n);
+extern double array_mean(int array[], int n);
 int main() {
     int arr[] = {1, 2, 5, 4, 8};
     std::cout << "Mean of array elements: " << array_mean(arr, 5) << std::endl;
---
+++ main-1-3.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+extern num_count(int array[], int n, int number);
+int main(){
+    int array[10] = {1,2,3,3,3,4,4,5,5,8};
+    int number = 3;
+    std::count << "the number of 3 is:"<<array[num_count(array,10,number)] << std::endl;
+
+}
