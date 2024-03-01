cff973db4276215efbf2c15068490a737de6c0fb
ZhaoyunMen <a1837712@adelaide.edu.au>
2024-03-01 22:52:00 +0800

    m

--- main-2-1.cpp
+++ main-2-1.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
-extern int min_element(int[], int ) ;
-int main(){
-    int array[] = {1,2,3,4,5}
-    std ::cout <<"the minimum element is" << min_element(array,5) << std ::endl;
-    return 0;
+extern int min_element(int array[], int n);
+int main() {
+    int array1[] = {1, 2, 3, 4, 5};
+    
+    std::cout << "minimum element is: " << min_element(array1, 5) << std::endl;
+    
+    return 0;
 }
