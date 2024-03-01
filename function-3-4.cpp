a3bbb3339d579b8d2e0c53940f366c839310b367
ZhaoyunMen <a1837712@adelaide.edu.au>
2024-03-01 13:50:21 +0800

    pra3-4

---
+++ function-3-4.cpp
@@ -0,0 +1,20 @@
+void print_print_fail(char grade){
+    switch(grade){
+        case 'A':
+        std::count <<"Pass"<< std ::endl;
+        break;
+        case 'B':
+        std::count <<"Pass"<< std ::endl;
+        break;
+        case 'C':
+        std::count <<"Pass"<< std ::endl;
+        break;
+        case 'D':
+        std::count <<"Fail"<< std ::endl:
+        break;
+        case 'F':
+        std::count <<"Fail"<< std ::endl;
+        default:
+        std::count<<"Nothing"<< std ::endl;
+    }
+}
---
+++ main-3-4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+extern void print_print_fail(char)
+
+int main() {
+    grade1 = 'A';
+    grade2 = 'F';
+    grade3 = 'G';
+
+    std::count <<print_print_fail(grade1) <<std ::endl;
+    std::count <<print_print_fail(grade2) <<std ::endl;
+    std::count <<print_print_fail(grade3) <<std ::endl;
+    return 0;
+}
