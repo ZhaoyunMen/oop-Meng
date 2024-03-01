53b853c2f4b17eceb0e0bef87e3ca5417c7652e9
ZhaoyunMen <a1837712@adelaide.edu.au>
2024-03-01 21:06:40 +0800

    change

--- main-1-3.cpp
+++ main-1-3.cpp
@@ -3,7 +3,7 @@ extern num_count(int[], int , int  );
 int main(){
     int array[] = {1,2,3,3,3,4,4,5,5,8};
     int number = 3;
-    std::count << "the number of 3 is:"<<num_count(array,10,number) << std::endl;
+    std:: << "the number of 3 is:"<<num_count(array,10,number) << std::endl;
     return 0;
 
 }
