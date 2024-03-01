bb9093324cce48cdd50a3c842e4116448f0f5fc6
ZhaoyunMen <a1837712@adelaide.edu.au>
2024-03-01 22:44:04 +0800

    m

--- main-1-3.cpp
+++ main-1-3.cpp
@@ -1,8 +1,10 @@
+
 #include <iostream>
-extern int num_count(int array[], int n, int number);
+int num_count(int array[], int n, int number);
 int main() {
-    int array[] = {1, 2, 3, 4, 5, 4, 3, 2, 1};
-    int number = 3;
-    std::cout << "Number of 3 are: " << num_count(array, 9, number) << std::endl;
-    return 0;
+    int array[] = {1, 3, 3, 4, 4, 5, 4, 6, 7, 8, 4};
+    int desiredNumber = 3;
+    int result = num_count(array,11 , desiredNumber);
+    std::cout << "Number of occurrences of " << desiredNumber << ": " << result << std::endl;
+    return 0;
 }
