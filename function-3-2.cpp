8bfff4a0744b137eaa2811023649fb037df1f3ce
ZhaoyunMen <a1837712@adelaide.edu.au>
2024-03-01 23:54:48 +0800

    m

--- function-3-2.cpp
+++ function-3-2.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 int median_array(int array[], int n){
-    if (n % 2 ==0 && n < 1){
+    if (n % 2 ==0){
+        return 0;
+    }
+    if (n < 1){
         return 0;
     }
     for (int i=0; i < n; i++) {
