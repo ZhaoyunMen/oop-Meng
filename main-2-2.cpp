01e8cefa43656b69a64a6f295065a844458b6207
ZhaoyunMen <a1837712@adelaide.edu.au>
2024-03-01 23:46:33 +0800

    m

--- main-2-2.cpp
+++ main-2-2.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 extern int max_element(int[], int) ;
 int main(){
-    int array1[] = {1,2,3,4,5}
-    int array2[] = {5,6,7,8,9}
-    std::cout <<"the Maxmum element is" << max_element(array1,5) << std ::endl;
+    int array1[] = {1,2,3,4,5};
+    int array2[] = {5,6,7,8,9};
+    std::cout <<"the Maxmum element is" << max_element(array1,5) << std::endl;
     std::cout << "Maxmum element is: " << max_element(array2, 5) << std::endl;
     return 0;
 }
