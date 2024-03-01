49398d3ad3b2eff567d6a30d9b1d02351b2001d7
ZhaoyunMen <a1837712@adelaide.edu.au>
2024-03-01 13:05:26 +0800

    pra3-1

---
+++ main-3-5,cpp.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+extern bool is_fanarray(int[], int )
+
+int main() {
+    int array1[] = {1, 2, 3, 2, 1};
+    
+
+    int array2[] = {5, 4, 3, 2, 1};
+    
+
+    
+    std::cout << "arr1 is fanarray? " << std::boolalpha << is_fanarray(array1, 5) << std::endl;
+    std::cout << "arr2 is fanarray? " << std::boolalpha << is_fanarray(array2, 5) << std::endl;
+
+    return 0;
+}
