6c8c839cb9285b7ff181c6d56de57f0cd4735193
ZhaoyunMen <a1837712@adelaide.edu.au>
2024-03-01 23:29:11 +0800

    m

--- main-2-3.cpp
+++ main-2-3.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 extern void two_five_nine(int [], int n);
 int main() {
-    int array[7] = {2, 5, 9, 2, 2, 2, 5};
-    two_five_nine(array, 7);
-    return 0;
+    int array[7] = {2, 5, 9, 2, 2, 2, 5};
+    two_five_nine(array, 7);
+    return 0;
 }
--- main-2-5.cpp
+++ main-2-5.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 extern bool is_descending(int array[], int n);
 int main() {
-    int array1[] = {5, 4, 3, 2, 1};
-    int array2[] = {1, 2, 3, 4, 5};
-    std::cout << "answer: " << is_descending(array1, 5) << std::endl;
-    std::cout << "answer: " << is_descending(array2, 5) << std::endl;
-    return 0;
+    int array1[] = {5, 4, 3, 2, 1};
+    int array2[] = {1, 2, 3, 4, 5};
+    std::cout << "answer: " << is_descending(array1, 5) << std::endl;
+    std::cout << "answer: " << is_descending(array2, 5) << std::endl;
+    return 0;
 }
