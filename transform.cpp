7d10c2c0b9e3915ac7d65ffd1cc65dcee9047038
ZhaoyunMen <a1837712@adelaide.edu.au>
2024-03-04 10:53:05 +0800

    wk2-1

---
+++ transform.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+int transform(int number){
+
+    Array[] = [];
+    if number < 1{
+        return 0
+    }
+    
+    
+     
+    while number >= 1 {
+        if number >1{
+            if number % 2 != 0
+                Array[].insert(0,1)
+            if number %2 == 0
+                Array[].insert(0,0)
+        }
+        if number == 1{
+            Array[].insert(0,0)
+            Array[].insert(0,1)
+            number = number - 1
+        }
+    }
+    return Array[]
+    
+}
