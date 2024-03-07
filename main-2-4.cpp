e2bdbea4575263c0fd57eff897c39e2247adce71
ZhaoyunMen <a1837712@adelaide.edu.au>
2024-03-07 22:44:29 +0800

    m

--- main-2-4.cpp
+++ main-2-4.cpp
@@ -10,13 +10,13 @@ int main() {
     int test_array[] = {5, 3, 9, 1, 7};
 
     
-    int length = sizeof(test_array) / sizeof(test_array[0]);
+    
 
     
-    std::cout << "Minimum value in the array: " << array_min(test_array, length) << std::endl;
+    std::cout << "Minimum value in the array: " << array_min(test_array, 5) << std::endl;
 
     
-    std::cout << "Maximum value in the array: " << array_max(test_array, length) << std::endl;
+    std::cout << "Maximum value in the array: " << array_max(test_array, 5) << std::endl;
 
     
     std::cout << "Sum of minimum and maximum values in the array: " << sum_min_max(test_array, length) << std::endl;
