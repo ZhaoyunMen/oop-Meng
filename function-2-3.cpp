c7dbbce3161c95721f227e7bee78cbc126eb4c84
ZhaoyunMen <a1837712@adelaide.edu.au>
2024-03-07 23:28:41 +0800

    m

--- function-2-3.cpp
+++ function-2-3.cpp
@@ -1,30 +1,26 @@
 #include <iostream>
 
 bool is_palindrome(int integers[], int length) {
-    if (length <= 0) {
-        return false;
+    if (length < 1) {
+        return  false;
     }
     for (int i = 0; i < length / 2; i++) {
         if (integers[i] != integers[length - i - 1]) {
              return false;
-        }
-        if (integers[i] > integers[i + 1]) {
-            return false;
-        } 
-        if (integers[i] == integers[length/2]) {
-            return false;
-        }
+        
     }
     return true;
 }
-
+}
 
 int sum_array_elements(int integers[], int length) {
-    if (length <= 0)
+
+    if (length < 1)
         return -1;
 
     int sum = 0;
     for (int i = 0; i < length; ++i) {
+
         sum += integers[i];
     }
     return sum;
