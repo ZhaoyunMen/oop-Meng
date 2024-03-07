74061a424faa767c7fb0222b45ac638d23f894f4
ZhaoyunMen <a1837712@adelaide.edu.au>
2024-03-07 23:10:43 +0800

    m

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -7,7 +7,7 @@ int binary_to_int(int binary_digits[], int number_of_digits) {
     int decimal = 0;
     int power = 0;
     for (int i = number_of_digits - 1; i >= 0; --i) {
-        if (binary_digits[i] == '1') {
+        if (binary_digits[i] == 1) {
             decimal += pow(2, power);
         }
         ++power;
