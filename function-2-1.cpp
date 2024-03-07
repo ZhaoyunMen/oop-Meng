6aa7f41eadc91c28fda30493f05811e9573ce4a9
ZhaoyunMen <a1837712@adelaide.edu.au>
2024-03-07 23:07:15 +0800

    m

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -1,35 +1,25 @@
 #include <iostream>
 #include <string>
 #include <algorithm> 
+
 using namespace std;
-void print_binary_str(std::string decimal_number){
+
+void print_binary_str(std::string decimal_number) {
+
     int number = std::stoi(decimal_number);
+    std::string binary = "";
 
-    std::string arr = "";
+    if (number == 0) {
 
-    if (number < 1){
-        std::cout << "0" << std::endl;
-        
-    }
-     
-    while (number >= 1) {
-        if (number > 1){
-            if (number % 2 == 1){
-                arr += "1";
-                
-            }
-            if (number %2 == 0){
-                arr += "0";
-            }
+        binary = "0";
+    } else {
+        while (number > 0) {
+
+            binary += std::to_string(number % 2);
+            number /= 2;
         }
-        if (number == 1){
-            arr += "0";
-            arr += "1";
-            number = number - 1;
-        }
-
     }
-    std::reverse(arr.begin(),arr.end());
-    std::cout<<arr<<std::endl;
+
+    std::reverse(binary.begin(), binary.end());
+    std::cout << binary << std::endl;
 }
-
