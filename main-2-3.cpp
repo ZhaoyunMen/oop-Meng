7a3f528ffe49a5df16856b529efb9e5272cb6fbb
ZhaoyunMen <a1837712@adelaide.edu.au>
2024-03-07 23:17:38 +0800

    m

--- main-2-3.cpp
+++ main-2-3.cpp
@@ -2,8 +2,9 @@
 extern bool is_palindrome(int integers[], int length);
 extern int sum_array_elements(int integers[], int length);
 extern int sum_if_palindrome(int integers[], int length);
+
 int main(){
-    int main() {
+    
     
     int arr1[] = {1, 2, 3, 2, 1}; 
     int arr2[] = {1, 2, 3, 4, 5}; 
@@ -15,5 +16,4 @@ int main(){
     std::cout << "Sum of non-palindrome array: " << sum2 << std::endl;
 
     return 0;
-}
 }
