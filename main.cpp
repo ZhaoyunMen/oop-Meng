1c7262712863dbe34ce3000d296ce08a26720e18
ZhaoyunMen <a1837712@adelaide.edu.au>
2024-03-11 19:06:11 +0800

    m

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include "workshop.h"
+#include <iostream>
+
+int main() {
+    
+    double value = 10.5;
+    std::cout << "Original value: " << value << std::endl;
+    changeValue(&value);
+    std::cout << "Changed value: " << value << std::endl;
+
+    
+    double arr[] = {1.1, 2.2, 3.3, 4.4, 5.5};
+    
+    std::cout << "Array elements: ";
+    printArray(arr, 5);
+    std::cout << std::endl;
+
+  
+    double max = arrayMax(arr, 5);
+    std::cout << "Max value in the array: " << max << std::endl;
+
+    
+    int arraySize = 5;
+    double initValue = 3.14;
+    double *dynamicArr = dynamicArray(arraySize, initValue);
+    std::cout << "Dynamic array elements initialized with " << initValue << ": ";
+    printArray(dynamicArr, arraySize);
+    std::cout << std::endl;
+
+    
+    delete[] dynamicArr;
+
+    return 0;
+}
