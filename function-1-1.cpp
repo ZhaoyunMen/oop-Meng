9099731b34b18eeafea23cc8d2d6fc70d5d9efbb
ZhaoyunMen <a1837712@adelaide.edu.au>
2024-03-15 13:46:20 +0800

    m

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
 double arrayMin(double* array, int size){
 
-    double* array = new double[size];
-    for (int i = 0; i < size; i++){
-        array[i] = i;
-    }
+    
     double min = array[0];
     for(int i = 0; i < size;i ++){
         if (array[i]<min){
--- main-1-1.cpp
+++ main-1-1.cpp
@@ -2,7 +2,7 @@
 extern double arrayMin(double* array, int size);
 int main(){
     double array[5] ={1,2,3,4,5};
-    delete[] arrayMin(array,5);
+    
     std::cout<<arrayMin(array,5)<<std::endl;
     
     return 0;
