da1a8340d025da753306d507e3bfd151a52d22b0
ZhaoyunMen <a1837712@adelaide.edu.au>
2024-03-25 10:08:57 +0800

    m

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -2,11 +2,10 @@
 #include "Musician.h"
 extern Musician(std::string instrument, int experience);
 int main() {
-Musician m1;
-std::cout << "Default instrument: " << m1.get_instrument() << ", Default
-experience: " << m1.get_experience() << std::endl;
-Musician m2("guitar", 5);
-std::cout << "Instrument: " << m2.get_instrument() << ", Experience: " <<
-m2.get_experience() << std::endl;
-return 0;
+
+    Musician m1;
+    std::cout << "Default instrument: " << m1.get_instrument() << ", Default experience: " << m1.get_experience() << std::endl;
+    Musician m2("guitar", 5);
+    std::cout << "Instrument: " << m2.get_instrument() << ", Experience: " <<m2.get_experience() << std::endl;
+    return 0;
 }
